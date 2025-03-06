bdea433f92867eeb4ca9f4f11aaa20aba9934af6
Daneshpm <danesh12112002@gmail.com>
2025-03-06 14:34:18 +0530

    data ingestion completed

---
+++ Networksecurity/entity/artifact_entity.py
@@ -0,0 +1,36 @@
+# This code snippet is defining several data classes using the `dataclass` decorator from the
+# `dataclasses` module in Python. Each class represents a specific artifact or object related to a
+# machine learning pipeline or data processing workflow. Here's a brief overview of each class:
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    trained_file_path:str
+    test_file_path:str
+
+# @dataclass
+# class DataValidationArtifact:
+#     validation_status: bool
+#     valid_train_file_path: str
+#     valid_test_file_path: str
+#     invalid_train_file_path: str
+#     invalid_test_file_path: str
+#     drift_report_file_path: str
+
+# @dataclass
+# class DataTransformationArtifact:
+#     transformed_object_file_path: str
+#     transformed_train_file_path: str
+#     transformed_test_file_path: str
+
+# @dataclass
+# class ClassificationMetricArtifact:
+#     f1_score: float
+#     precision_score: float
+#     recall_score: float
+    
+# @dataclass
+# class ModelTrainerArtifact:
+#     trained_model_file_path: str
+#     train_metric_artifact: ClassificationMetricArtifact
+#     test_metric_artifact: ClassificationMetricArtifact
---
+++ main.py
@@ -0,0 +1,49 @@
+# This code snippet is a Python script that seems to be part of a network security application. Here's
+# a breakdown of what it does:
+from Networksecurity.components.data_ingestion import DataIngestion
+# from Networksecurity.components.data_validation import DataValidation
+# from Networksecurity.components.data_transformation import DataTransformation
+from Networksecurity.exception.exception import NetworkSecurityException
+from Networksecurity.logging.logger import logging
+from Networksecurity.entity.config_entity import DataIngestionConfig
+from Networksecurity.entity.config_entity import TrainingPipelineConfig
+
+# from Networksecurity.components.model_trainer import ModelTrainer
+# from Networksecurity.entity.config_entity import ModelTrainerConfig
+ 
+
+import sys
+
+if __name__=='__main__':
+    try:
+        trainingpipelineconfig=TrainingPipelineConfig()
+        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion=DataIngestion(dataingestionconfig)
+        logging.info("Initiate the data ingestion")
+        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
+        print(dataingestionartifact)
+        # data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        # data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        # logging.info("Initiate the data Validation")
+        # data_validation_artifact=data_validation.initiate_data_validation()
+        # logging.info("data Validation Completed")
+        # print(data_validation_artifact)
+        # data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        # logging.info("data Transformation started")
+        # data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        # data_transformation_artifact=data_transformation.initiate_data_transformation()
+        # print(data_transformation_artifact)
+        # logging.info("data Transformation completed")
+
+        # logging.info("Model Training sstared")
+        # model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        # model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        # model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        # logging.info("Model Training artifact created")
+        
+        
+        
+    except Exception as e:
+           raise NetworkSecurityException(e,sys)
