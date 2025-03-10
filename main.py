0286664a54e506aadd9772c8f60f4cb11bf93334
Daneshpm <danesh12112002@gmail.com>
2025-03-10 14:51:20 +0530

    datatransformation completed

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 # a breakdown of what it does:
 from Networksecurity.components.data_ingestion import DataIngestion
 from Networksecurity.components.data_validation import DataValidation
-# from Networksecurity.components.data_transformation import DataTransformation
+from Networksecurity.components.data_transformation import DataTransformation
 from Networksecurity.exception.exception import NetworkSecurityException
 from Networksecurity.logging.logger import logging
-from Networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from Networksecurity.entity.config_entity import DataIngestionConfig, DataTransformationConfig, DataValidationConfig
 from Networksecurity.entity.config_entity import TrainingPipelineConfig
 
 # from Networksecurity.components.model_trainer import ModelTrainer
@@ -29,12 +29,12 @@ if __name__=='__main__':
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
         print(data_validation_artifact)
-        # data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
-        # logging.info("data Transformation started")
-        # data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
-        # data_transformation_artifact=data_transformation.initiate_data_transformation()
-        # print(data_transformation_artifact)
-        # logging.info("data Transformation completed")
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
 
         # logging.info("Model Training sstared")
         # model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
